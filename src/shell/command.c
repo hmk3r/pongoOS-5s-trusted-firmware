d2d14e661fa3ba42109d53a90f462b2b4ee13131
Konrad Dybcio <konrad.dybcio@somainline.org>
2022-06-11 19:15:24 +0200

    shell/command: Add dumpusb for linuxstuff

--- src/shell/command.c
+++ src/shell/command.c
@@ -225,6 +225,99 @@ void dump_system_regs() {
     lock_release(&command_lock);
 }
 
+extern uint64_t gSynopsysBase;
+#define DWC2_REG_VAL(x) *(volatile uint32_t *)(gSynopsysBase + x)
+
+void dwc2_force_host_mode(bool want_host_mode) {
+    unsigned long long GUSBCFG = DWC2_REG_VAL(0xc);
+
+    int set = want_host_mode ? 29 : 30;
+    int clear = want_host_mode ? 30 : 29;
+
+    GUSBCFG &= ~(1ULL << clear);
+    GUSBCFG |= (1ULL << set);
+
+    *(volatile uint32_t *)(gSynopsysBase + 0xc) = GUSBCFG;
+
+    sleep(1); // life is going so fast, take a single second to think about your choices and let dwc2 do the same.
+}
+
+void dump_usb_regs() {
+    lock_take(&command_lock);
+
+    unsigned long long GRXFSIZ = DWC2_REG_VAL(0x24);
+    unsigned long long GNPTXFSIZ = DWC2_REG_VAL(0x28);
+    unsigned long long GHWCFG1 = DWC2_REG_VAL(0x44);
+    unsigned long long GHWCFG2 = DWC2_REG_VAL(0x48);
+    unsigned long long GHWCFG3 = DWC2_REG_VAL(0x4c);
+    unsigned long long GHWCFG4 = DWC2_REG_VAL(0x50);
+    unsigned long long HPTXFSIZ = DWC2_REG_VAL(0x100);
+    unsigned long long GSNPSID = DWC2_REG_VAL(0x400);
+
+    // The order matches the one in /sys/kernel/debug/usb/devicename/hw_params
+    iprintf("-------- DWC2 hw_params --------\n");
+
+    iprintf("op_mode = 0x%llx\n", (GHWCFG2 & 0x7) >> 0);
+
+    iprintf("arch = 0x%llx\n", (GHWCFG2 & (0x3 << 3)) >> 3);
+
+    iprintf("dma_desc_enable = 0x%llx\n", GHWCFG4 & (1 << 30));
+
+    iprintf("enable_dynamic_fifo = 0x%llx\n", GHWCFG2 & (1 << 19));
+
+    iprintf("en_multiple_tx_fifo = 0x%llx\n", GHWCFG4 & (1 << 25));
+
+    iprintf("rx_fifo_size (dont mistake with something_rx_fifo_size) = %llu\n", GRXFSIZ & 0xffff);
+
+    /* force host mode to make the readout sane */
+    dwc2_force_host_mode(true);
+    iprintf("host_nperio_tx_fifo_size = %llu\n", (GNPTXFSIZ & (0xffff << 16)) >> 16);
+
+    /* force dev mode to make the readout sane */
+    dwc2_force_host_mode(false);
+    iprintf("dev_nperio_tx_fifo_size = %llu\n", (GNPTXFSIZ & (0xffff << 16)) >> 16);
+
+    iprintf("host_perio_tx_fifo_size = %llu\n", (HPTXFSIZ & (0xffff << 16)) >> 16);
+
+    iprintf("nperio_tx_q_depth = %llu\n", (GHWCFG2 & (0x3 << 22)) >> 22 << 1);
+
+    iprintf("host_perio_tx_q_depth = %llu\n", (GHWCFG2 & (0x3 << 24)) >> 24 << 1);
+
+    iprintf("dev_token_q_depth = %llu\n", (GHWCFG2 & (0x1f << 26)) >> 26);
+
+    unsigned long long width = (GHWCFG3 & 0xf);
+
+    iprintf("max_transfer_size = %d\n", (1 << (width + 11)) - 1);
+
+    width = ((GHWCFG3 & (0x7 << 4)) >> 4);
+
+    iprintf("max_packet_size = %d\n", (1 << (width + 4)) - 1);
+
+    iprintf("host_channels = %llu\n", 1 + ((GHWCFG2 & (0xf << 14)) >> 14));
+
+    iprintf("hs_phy_type = %llu\n", ((GHWCFG2 & (0x3 << 6)) >> 6));
+
+    iprintf("fs_phy_type = %llu\n", ((GHWCFG2 & (0x3 << 8)) >> 8));
+
+    iprintf("i2c_enable = %llu\n", (GHWCFG3 & (1 << 8)));
+
+    iprintf("num_dev_ep = %llu\n", ((GHWCFG2 & (0xf << 10))) >> 10);
+
+    iprintf("num_dev_perio_in_ep = %llu\n", (GHWCFG4 & (0xf)));
+
+    iprintf("total_fifo_size = %llu\n", ((GHWCFG3 & (0xffff << 16))) >> 16);
+
+    iprintf("power_optimized = %llu\n", GHWCFG4 & (1 << 4));
+
+    iprintf("utmi_phy_data_width = %llu\n", (GHWCFG4 & (0x3 << 14)) >> 14);
+
+    iprintf("snpsid = 0x%llx\n", GSNPSID);
+
+    iprintf("dev_ep_dirs = 0x%llx\n", GHWCFG1);
+
+    lock_release(&command_lock);
+}
+
 void fix_a7() {
     __asm__ volatile(
         // "unlock the core for debugging"
@@ -278,6 +371,7 @@ void command_init() {
     command_task->flags |= TASK_RESTART_ON_EXIT;
     command_task->flags &= ~TASK_CAN_EXIT;
     command_register("help", "shows this help message", help);
+    command_register("dumpusb", "dumps various dwc2 registers for lee noocks", dump_usb_regs);
     command_register("dump", "dumps various system registers", dump_system_regs);
     command_register("fix", "tries to fix a7..", fix_a7);
 }
